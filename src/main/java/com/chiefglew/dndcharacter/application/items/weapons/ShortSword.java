dc8e40b136140712742cadfe6cb97358ff443e24
thomas.glew <thomas.glew@fdmgroup.com>
2018-03-14 17:11:54 +0000

    short sword doesn't need to have those methods anymore

--- src/main/java/com/chiefglew/dndcharacter/application/items/weapons/ShortSword.java
+++ src/main/java/com/chiefglew/dndcharacter/application/items/weapons/ShortSword.java
@@ -1,32 +1,12 @@
 package com.chiefglew.dndcharacter.application.items.weapons;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.chiefglew.dndcharacter.application.items.Item;
-import com.chiefglew.dndcharacter.application.items.itemusecommands.ItemUseCommand;
-
 @Component
 @Scope("prototype")
-public class ShortSword extends Item implements MartialMeleeWeapon{
+public class ShortSword extends MartialWeapon{
 
-	private ItemUseCommand useCommand;
-    
-	@Autowired
-    public ShortSword(ItemUseCommand itemUseCommand){
-		this.useCommand = itemUseCommand;
-		useCommand.addDamage(dice); // need to do something here... or somewhere else....
-    }
 
-	@Override
-	public ItemUseCommand useItem() {
-		return this.useCommand;
-	}
-
-	@Override
-	public int getNumberOfUses() {
-		return -1;
-	}
 	
 }
