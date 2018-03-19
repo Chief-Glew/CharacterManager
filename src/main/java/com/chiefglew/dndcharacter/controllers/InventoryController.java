005ae2b29f8dd3c87b87e48567f0a459624be2d8
thomas.glew <thomas.glew@fdmgroup.com>
2018-03-19 09:17:25 +0000

    stupid messenger thing

--- src/main/java/com/chiefglew/dndcharacter/controllers/InventoryController.java
+++ src/main/java/com/chiefglew/dndcharacter/controllers/InventoryController.java
@@ -1,25 +1,42 @@
 package com.chiefglew.dndcharacter.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InventoryController {
 
 
-	
+	List<String> messages = new ArrayList<String>();
 	
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String showInventories(Model model) {
 		return "index";
 	}
 	
-	@RequestMapping(value = { "/{inventoryId}/" }, method = RequestMethod.GET)
-	public String showInventoryItems(@PathVariable long inventoryId, Model model){
-		model.addAttribute("iventoryNumber" , inventoryId);
-		return "showInventoryItems";
+//	@RequestMapping(value = { "/{inventoryId}/" }, method = RequestMethod.GET)
+//	public String showInventoryItems(@PathVariable long inventoryId, Model model){
+//		model.addAttribute("iventoryNumber" , inventoryId);
+//		return "showInventoryItems";
+//	}
+	
+	@RequestMapping(value = { "/messenger/" }, method = RequestMethod.POST)
+	public String showInventoryItems(@RequestParam("message") String message, Model model){
+		this.messages.add(message);
+		model.addAttribute("messages" , this.messages);
+		return "messenger";
+	}
+	
+	@RequestMapping(value = { "/messenger/" }, method = RequestMethod.GET)
+	public String showInventoryItems(Model model){
+		model.addAttribute("messages" , messages);
+		return "messenger";
 	}
 }
