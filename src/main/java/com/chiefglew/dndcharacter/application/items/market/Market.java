c8f1f90fa6d9adef29f783f053255eba4a9ac032
thomas.glew <thomas.glew@fdmgroup.com>
2018-03-23 14:07:14 +0000

    started implementing last test

--- src/main/java/com/chiefglew/dndcharacter/application/items/market/Market.java
+++ src/main/java/com/chiefglew/dndcharacter/application/items/market/Market.java
@@ -1,6 +1,7 @@
 package com.chiefglew.dndcharacter.application.items.market;
 
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.chiefglew.dndcharacter.application.items.Item;
@@ -15,6 +16,7 @@ public class Market extends GenericMarket<Item, Integer> {
 
 	@Override
 	public GenericTrade<Item, Item> buyItem(String itemKey, GenericTrade<Item, Item> trade) throws OutOfStockException {
+		
 		Map<String, Integer> cost;
 		try {
 			cost = appraise(itemKey);
@@ -22,13 +24,27 @@ public class Market extends GenericMarket<Item, Integer> {
 			e.printStackTrace();
 			throw new OutOfStockException(itemKey);
 		}
-		Map<Item, Integer> selling = trade.getSelling();
 		
-		for (Item item : selling.keySet()) {
-			stock.addItemToStock(item.getValuableName(), item, selling.get(item));
+		if (tradeContainsValuablesToCoverTheCost(trade, cost)){
+			Map<Item, Integer> itemCost = pullIemsFromCostOutOfTrade(trade, cost);
+			for (Item item : itemCost.keySet()) {
+				stock.addItemToStock(item.getValuableName(), item, itemCost.get(item));
+			}
+			trade.setItem(stock.withdrawFromStock(itemKey));
 		}
-		trade.setItem(stock.withdrawFromStock(itemKey));
+
 		return trade;
+	}
+
+	private Map<Item, Integer> pullIemsFromCostOutOfTrade(GenericTrade<Item, Item> trade, Map<String, Integer> cost) {
+		Map<Item, Integer> returnMap = new HashMap<Item, Integer>();
+		
+		return returnMap;
+	}
+
+	private boolean tradeContainsValuablesToCoverTheCost(GenericTrade<Item, Item> trade, Map<String, Integer> cost) {
+		
+		return false;
 	}
 
  
