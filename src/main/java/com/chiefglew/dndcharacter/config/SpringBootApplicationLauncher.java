b3d92fe04cf13c97c96758a8fb2598749fe73c50
Chief-Glew <t.glew@hotmail.co.uk>
2018-04-02 12:59:17 +0100

    added a spring boot launcher

---
+++ src/main/java/com/chiefglew/dndcharacter/config/SpringBootApplicationLauncher.java
@@ -0,0 +1,13 @@
+package com.chiefglew.dndcharacter.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationLauncher.class, args);
+	}
+
+}
